f762828ce99b6253a2cd994a25c40081776c694d
Philihp Busby <philihp@gmail.com>
2020-04-20 06:59:32 +0000

    Release v1.0.0
    I'm pretty satisfied with this as it is. It's stable and has 100%
    test coverage, so bumping to 1.0.0 to indicate it's ready for others.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Openskill.MixProject do
   def project do
     [
       app: :openskill,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.9",
       description: description(),
       start_permanent: Mix.env() == :prod,
@@ -12,7 +12,7 @@ defmodule Openskill.MixProject do
       package: [
         maintainers: ["Philihp Busby"],
         licenses: ["MIT"],
-        links: %{github: "https://github.com/philihp/openskill"}
+        links: %{GitHub: "https://github.com/philihp/openskill"}
       ],
       test_coverage: [tool: ExCoveralls]
     ]
